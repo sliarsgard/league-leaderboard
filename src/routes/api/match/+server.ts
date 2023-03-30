19cab87776e071d949a65b411b81ceac0fd4e44b
Simon <simonliarsgard99@gmail.com>
2023-03-30 13:58:00 +0200

    Added matchmaking feature

---
+++ src/routes/api/match/+server.ts
@@ -0,0 +1,62 @@
+import {error} from '@sveltejs/kit'
+import supabase from '$lib/supabase';
+import type { RequestHandler } from './$types';
+import type { Player } from '$lib/types';
+
+export const POST = (async ({ request }) => {
+    const {playerIds}: {playerIds: number[]} = await request.json();
+    
+    const response = await supabase.from('players').select('*').in('id', playerIds);
+    if (response.error) throw error(500, 'An error occurred while fetching players.');
+
+    const teams = generateTeams(response.data);
+
+	return new Response(JSON.stringify(teams));
+}) satisfies RequestHandler;
+
+const generateCombinations = <T>(arr: T[], size: number): T[][] => {
+    const result: T[][] = [];
+    const f = (active: T[], rest: T[], depth: number) => {
+        if (depth === 0) {
+            result.push(active);
+            return;
+        }
+
+        for (let i = 0; i < rest.length; i++) {
+            const current = rest[i];
+            const newActive = [...active, current];
+            const newRest = rest.slice(i + 1);
+            f(newActive, newRest, depth - 1);
+        }
+    }
+
+    f([], arr, size);
+    return result;
+}
+
+const generateTeams = (players: Player[]): [Player[], Player[]] => {
+    if (players.length !== 10)
+        throw error(400, 'Invalid number of players.');
+    
+    const teamCombinations = generateCombinations(players, 5);
+
+    let optimalTeamA: Player[] = [];
+    let optimalTeamB: Player[] = [];
+    let minEloDifference = Infinity;
+
+    for (const teamA of teamCombinations) {
+        const teamB = players.filter((p) => !teamA.includes(p));
+        const eloDifference = Math.abs(
+            teamA.reduce((acc, p) => acc + p.elo, 0) -
+                teamB.reduce((acc, p) => acc + p.elo, 0)
+        )
+
+        if (eloDifference < minEloDifference) {
+            minEloDifference = eloDifference;
+            optimalTeamA = teamA;
+            optimalTeamB = teamB;
+        }
+    }
+
+    return [optimalTeamA, optimalTeamB];
+}
---
+++ src/routes/match/+page.server.ts
@@ -0,0 +1,15 @@
+import supabase from '$lib/supabase';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+	const players = await supabase.from('players').select('*')
+	if (!players.data) throw error(500, 'Supabase error')
+	return {
+		players: players.data.sort((a, b) => {
+            if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+            if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+            return 0;
+        }),
+	};
+}) satisfies PageServerLoad;
